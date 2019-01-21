b217d7a8f10a724cb0be7de64c3d7f3193ff0fd9
Michal Matlega <m1sz3lek@gmail.com>
2019-01-21 20:48:00 +0100

    routing

--- app.js
+++ app.js
@@ -39,41 +39,9 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-var MongoClient = require('mongodb').MongoClient;
-const Snapshot = require('./models').Snapshot;
 
-Snapshot.findOne({
-    attributes: ['time'],
-    order: [['time','DESC']]
-}).then((res) => {
-    let fromTime = res.time;
-    MongoClient.connect(process.env.MONGODB_URI)
-        .then(function (client) { // <- db as first argument
-            let db = client.db();
-            const collection = db.collection('weather');
-            collection.find({'currently.time': {$gt: fromTime}},{projection: {'currently': 1, '_id': 0}}).toArray(function(err, docs) {
-                let snapshots = docs.map((doc) => {
-                    return doc.currently;
-                });
 
-                Snapshot
-                    .bulkCreate(snapshots)
-                    .then(() => {
-                        console.log("Weather saved.")
-                    })
-                    .catch(() => {
-                        console.log("Something went wrong. Weather not saved.")
-                    });
-            });
 
-        })
-        .catch(function (err) {
-            console.log(err);
-        });
-}).catch((e) => {
-    console.log('Error during fetching fromTime');
-    console.log(e);
-});
 
 
 
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
 
+var MongoClient = require('mongodb').MongoClient;
+const Snapshot = require('../models').Snapshot;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    Snapshot.findOne({
+        attributes: ['time'],
+        order: [['time', 'DESC']]
+    }).then((res) => {
+        let fromTime = res.time;
+        MongoClient.connect(process.env.MONGODB_URI)
+            .then(function (client) { // <- db as first argument
+                let db = client.db();
+                const collection = db.collection('weather');
+                collection.find({'currently.time': {$gt: fromTime}}, {
+                    projection: {
+                        'currently': 1,
+                        '_id': 0
+                    }
+                }).toArray(function (err, docs) {
+                    let snapshots = docs.map((doc) => {
+                        return doc.currently;
+                    });
+
+                    Snapshot
+                        .bulkCreate(snapshots)
+                        .then(() => {
+                            console.log("Weather saved.")
+                        })
+                        .catch(() => {
+                            console.log("Something went wrong. Weather not saved.")
+                        });
+                });
+
+            })
+            .catch(function (err) {
+                console.log(err);
+            });
+    }).catch((e) => {
+        console.log('Error during fetching fromTime');
+        console.log(e);
+    });
 });
 
 module.exports = router;
