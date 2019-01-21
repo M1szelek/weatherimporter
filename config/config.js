63c1a5e0a1020b675a147cd736d931d091474916
Michal Matlega <m1sz3lek@gmail.com>
2019-01-21 21:45:50 +0100

    mysql

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@ const fs = require('fs');
 
 module.exports = {
     development: {
-        username: 'root',
-        password: null,
-        database: 'weather_development',
-        host: '127.0.0.1',
+        username: process.env.DB_USERNAME,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOSTNAME,
         dialect: 'mysql'
     },
     test: {
